5986cd569b50563c950ed6fcd5bbca0adc18860e
touhouota <touhouota.register@gmail.com>
2018-01-16 13:00:31 +0900

    ログインの基本的な処理を追加

--- ruby/server.rb
+++ ruby/server.rb
@@ -2,5 +2,31 @@
 require 'cgi'
 require 'csv'
 require 'json'
+load './process.rb'
 
 print "Content-Type: text/plain;charset=utf-8\n\n"
+
+begin
+  cgi = CGI.new
+  # キーをSymbolに変更
+  # cgiをハッシュに変換する
+  parameter = cgi.params.map { |key, value| [key.to_sym, value.first] }.to_h
+
+  result = case parameter[:cmd]
+           when 'check_account'
+    check_account(parameter)
+           else
+    { ok: false, data: { message: 'そんなコマンド無いよ' } }
+  end
+
+  print JSON.generate(result)
+rescue StandardError => e
+  # errorの時は、エラー内容と実際のcgiを返す
+  response = {
+    ok: false,
+    data: e.inspect.to_s,
+    path: e.backtrace.join("\n"),
+    input: parameter
+  }
+  print JSON.generate(response)
+end
