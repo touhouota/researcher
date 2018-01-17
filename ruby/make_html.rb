7dec864f27227fa3112fa88385119d0575ba101b
touhouota <touhouota.register@gmail.com>
2018-01-18 02:25:23 +0900

    テーブルを作るスクリプトから、formを埋め込むようにした

--- ruby/make_html.rb
+++ ruby/make_html.rb
@@ -4,10 +4,37 @@ require 'erb'
 users = CSV.read('./list/users.csv').to_h
 user_id = nil
 
-table = CSV.table('./list/tasks.csv')
+table = CSV.table('./list/progress_table.csv')
 # 　配列に変換して転置
 user_task = table.to_a.transpose
 header = user_task.shift
+
+form = <<FORM
+
+<form class="task_form hide">
+  <p>
+    <label>
+      目標時間： <input type="number" name="minute" value="" step="5">分
+    </label>
+  </p>
+  <p>
+    <label>
+      進捗度：
+      <button type="button" class="minus">ー</button>
+      <progress value="0" max="100" class="progress" name="progress_bar"></progress>
+      <button type="button" class="plus">＋</button>
+    </label>
+  </p>
+  <p>
+    <label>
+      メモ: <br>
+      <textarea name="memo" rows="8" cols="80"></textarea>
+    </label>
+  </p>
+  <input type="hidden" name="plan_id" value="">
+</form>
+
+FORM
 
 html = <<TEMPLATE
 <thead><tr>
@@ -24,7 +51,9 @@ html = <<TEMPLATE
       <% user_id = item.to_s %>
       <td class="<%= user_id %>"> <%= users[user_id] %> </td>
     <% else %>
-      <td data-task="<%= header[idx] %>" class="<%= user_id %>"> <%= item %> </td>
+      <td data-task="<%= header[idx] %>" class="<%= user_id %>">
+      <%= form %>
+      </td>
     <% end %>
   <% end %>
   </tr>
