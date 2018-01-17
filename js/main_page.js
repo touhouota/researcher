4341849bd32f3500ebb93e0e5a7cbe54d56ce2e1
touhouota <touhouota.register@gmail.com>
2018-01-18 02:24:10 +0900

    情報を取得し、それらの情報をDOMに追加する処理を追加

--- js/main_page.js
+++ js/main_page.js
@@ -11,6 +11,8 @@ window.onload = function() {
 	let my_areas = document.getElementsByClassName(Base.get_cookie("user_id"));
 	let i = 0,
 		length = my_areas.length;
+	let event_targets, j = 0,
+		target_size;
 
 	for (i = 1; i < length; i += 1) {
 		button = template_button.cloneNode(true).querySelector("button");
@@ -22,5 +24,46 @@ window.onload = function() {
 		// 進捗更新ボタンのイベントを定義
 		my_areas[i].querySelector(".plus").addEventListener("click", Events.progress_plus);
 		my_areas[i].querySelector(".minus").addEventListener("click", Events.progress_minus);
+
+		event_targets = my_areas[i].querySelectorAll("input[type=number], textarea");
+		for (j = 0, target_size = event_targets.length; j < target_size; j += 1) {
+			console.log(event_targets[i]);
+			event_targets[j].addEventListener("change", Events.form_change);
+		}
 	}
+
+	// タスクの情報を取得する
+	let query = "?cmd=get_task";
+	Base.create_request("GET", Base.request_path + query, function() {
+		let response = JSON.parse(this.responseText);
+		console.table(response);
+
+		let tasks = response.data;
+		let i = 0,
+			size = tasks.length;
+		let search_query, form;
+		console.log(size);
+		for (i = 0; i < size; i += 1) {
+			search_query = [
+				".",
+				tasks[i].user_id,
+				"[data-task=", tasks[i].task, "]",
+				" > .task_form"
+			].join("");
+			// それぞれのformに情報を付加する
+			form = document.querySelector(search_query);
+			form.minute.value = tasks[i].expected;
+			form.querySelector(".progress").value = tasks[i].progress;
+			form.memo.value = tasks[i].memo;
+			form.plan_id.value = tasks[i].plan_id;
+
+			// もし、自分のタスクに何らかの情報があれば、「やるぞ！」ボタンを非表示にする
+			if (tasks[i].user_id === Base.get_cookie("user_id") && tasks[i].start) {
+				// formを表示
+				Display.at(form);
+				// ボタンを非表示
+				Display.hide_element(form.parentElement.querySelector(".add_task"));
+			}
+		}
+	}).send(null);
 }
