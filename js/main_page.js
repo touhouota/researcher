38b99c81fff9f2a675083ed8946cce4cf70ab58b
touhouota <touhouota.register@gmail.com>
2018-01-18 17:54:28 +0900

    onload時の処理を別ファルに行こう

--- js/main_page.js
+++ js/main_page.js
@@ -36,34 +36,9 @@ window.onload = function() {
 	let query = "?cmd=get_task";
 	Base.create_request("GET", Base.request_path + query, function() {
 		let response = JSON.parse(this.responseText);
-		console.table(response);
-
-		let tasks = response.data;
-		let i = 0,
-			size = tasks.length;
-		let search_query, form;
-		console.log(size);
-		for (i = 0; i < size; i += 1) {
-			search_query = [
-				".",
-				tasks[i].user_id,
-				"[data-task=", tasks[i].task, "]",
-				" > .task_form"
-			].join("");
-			// それぞれのformに情報を付加する
-			form = document.querySelector(search_query);
-			form.minute.value = tasks[i].expected;
-			form.querySelector(".progress").value = tasks[i].progress;
-			form.memo.value = tasks[i].memo;
-			form.plan_id.value = tasks[i].plan_id;
-
-			// もし、自分のタスクに何らかの情報があれば、「やるぞ！」ボタンを非表示にする
-			if (tasks[i].user_id === Base.get_cookie("user_id") && tasks[i].start) {
-				// formを表示
-				Display.at(form);
-				// ボタンを非表示
-				Display.hide_element(form.parentElement.querySelector(".add_task"));
-			}
+		console.table(response.data);
+		if (response.ok) {
+			TaskForm.insert_info(response.data);
 		}
 	}).send(null);
 }
