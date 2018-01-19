543d590f60bf090bef854b49a816bd6f2a09e850
touhouota <touhouota.register@gmail.com>
2018-01-19 16:31:45 +0900

    状態に応じて色分けをするための、処理を追加

--- js/display.js
+++ js/display.js
@@ -1,9 +1,31 @@
 let Display = {
-	at: function(element) {
-		element.classList.remove("hide");
-	},
+    at: function(element) {
+        element.classList.remove("hide");
+    },
 
-	hide_element: function(element) {
-		element.classList.add("hide");
-	}
+    hide_element: function(element) {
+        element.classList.add("hide");
+    },
+
+    progress_to_color: function(change_area, progress) {
+        console.log("change_area:", change_area, progress);
+        // 一旦、付いている色に関するクラスを取り除く
+        change_area.classList.remove("todo", "doing_first", "doing_second", "done");
+        switch (progress.toString()) {
+            case "0":
+                change_area.classList.add("todo");
+                break;
+            case "20":
+            case "40":
+                change_area.classList.add("doing_first");
+                break;
+            case "60":
+            case "80":
+                change_area.classList.add("doing_second");
+                break;
+            case "100":
+                change_area.classList.add("done");
+                break;
+        }
+    },
 };
--- js/event.js
+++ js/event.js
@@ -1,93 +1,101 @@
 let Events = {
-	create_form: function(event) {
-		let target_area = Base.parents(event.target, Base.get_cookie("user_id"));
-		let form = target_area.querySelector(".task_form");
-		// タスクformを表示
-		Display.at(form);
-		// 「やるぞ」ボタンの非表示
-		Display.hide_element(target_area.querySelector(".add_task"));
+    create_form: function(event) {
+        let target_area = Base.parents(event.target, Base.get_cookie("user_id"));
+        let form = target_area.querySelector(".task_form");
+        // タスクformを表示
+        Display.at(form);
+        // 「やるぞ」ボタンの非表示
+        Display.hide_element(target_area.querySelector(".add_task"));
 
-		let query = [
-			"cmd=start_task",
-			"&user_id=" + Base.get_cookie("user_id"),
-			"&plan_id=" + form.plan_id.value,
-		].join("");
+        let query = [
+            "cmd=start_task",
+            "&user_id=" + Base.get_cookie("user_id"),
+            "&plan_id=" + form.plan_id.value,
+        ].join("");
 
-		// 
-		Base.create_request("POST", Base.request_path, function() {
-			let response = JSON.parse(this.responseText);
-			console.table(response);
-		}).send(query);
+        //
+        Base.create_request("POST", Base.request_path, function() {
+            let response = JSON.parse(this.responseText);
+            console.table(response);
+        }).send(query);
 
-	},
+    },
 
-	form_change: function(event) {
-		console.log("form_change: ", event.target.value);
-		let form = Base.parents(event.target, "task_form");
-		let formdata = new FormData(form);
-		formdata.append("progress", form.querySelector(".progress").value);
+    form_change: function(event) {
+        console.log("form_change: ", event.target.value);
+        let form = Base.parents(event.target, "task_form");
+        let formdata = new FormData(form);
+        formdata.append("progress", form.querySelector(".progress").value);
 
-		Events.send_task_info(formdata);
-	},
+        Events.send_task_info(formdata);
+    },
 
-	progress_plus: function(event) {
-		let button = Base.parents(event.target, "plus");
-		let form = Base.parents(button.parentElement, "task_form");
-		let progress = form.querySelector(".progress");
+    progress_plus: function(event) {
+        let button = Base.parents(event.target, "plus");
+        let form = Base.parents(button.parentElement, "task_form");
+        let progress = form.querySelector(".progress");
 
-		let value = Number(progress.value);
-		console.log(progress);
-		if (value < 100) {
-			value += 20;
-		}
+        let value = Number(progress.value);
+        console.log(progress);
+        if (value < 100) {
+            value += 20;
+        }
 
-		// 値を指定する
-		progress.value = value;
-		let formdata = new FormData(form);
-		formdata.append("progress", value);
-		Events.send_task_info(formdata);
-	},
+        // 値を指定する
+        progress.value = value;
 
-	progress_minus: function(event) {
-		let button = Base.parents(event.target, 'minus');
-		let form = Base.parents(button.parentElement, "task_form");
-		let progress = form.querySelector(".progress");
+        // 更新をサーバへ送る
+        let formdata = new FormData(form);
+        formdata.append("progress", value);
+        Events.send_task_info(formdata);
 
-		let value = Number(progress.value);
-		if (0 < value) {
-			value -= 20;
-		}
+        // 色を変更
+        Display.progress_to_color(form.parentElement, value);
+    },
 
-		console.log(value);
+    progress_minus: function(event) {
+        let button = Base.parents(event.target, 'minus');
+        let form = Base.parents(button.parentElement, "task_form");
+        let progress = form.querySelector(".progress");
 
-		// 値の指定
-		progress.value = value;
-		let formdata = new FormData(form);
-		formdata.append("progress", value);
-		Events.send_task_info(formdata);
-	},
+        let value = Number(progress.value);
+        if (0 < value) {
+            value -= 20;
+        }
 
-	// 変更をサーバへ送る
-	// input: {cmd=xxxx, user_id=xxxx, plan_id=xxxx}
-	send_task_info: function(formdata) {
-		formdata.append("cmd", "task_modify");
-		formdata.append("user_id", Base.get_cookie("user_id"));
+        // 値の指定
+        progress.value = value;
 
-		Base.create_request("POST", Base.request_path, function() {
-			let response = JSON.parse(this.responseText);
-			if (response.ok) {
-				let task_data = response.data;
-				let search_query = [
-					"input[value='",
-					task_data.plan_id,
-					"']",
-				].join("");
-				let form = Base.parents(document.querySelector(search_query), "task_form");
-				console.log(form);
-				form.minute.value = task_data.expected;
-				form.querySelector(".progress").value = task_data.progress;
-				form.memo.value = task_data.memo;
-			}
-		}).send(formdata);
-	},
+        // 更新をサーバへ送る
+        let formdata = new FormData(form);
+        formdata.append("progress", value);
+        Events.send_task_info(formdata);
+
+        // 色を変更
+        Display.progress_to_color(form.parentElement, value);
+    },
+
+    // 変更をサーバへ送る
+    // input: {cmd=xxxx, user_id=xxxx, plan_id=xxxx}
+    send_task_info: function(formdata) {
+        formdata.append("cmd", "task_modify");
+        formdata.append("user_id", Base.get_cookie("user_id"));
+
+        Base.create_request("POST", Base.request_path, function() {
+            let response = JSON.parse(this.responseText);
+            if (response.ok) {
+                let task_data = response.data;
+                let search_query = [
+                    "input[value='",
+                    task_data.plan_id,
+                    "']",
+                ].join("");
+                let form = Base.parents(document.querySelector(search_query), "task_form");
+                console.log(form);
+                form.minute.value = task_data.expected;
+                form.querySelector(".progress").value = task_data.progress;
+                form.memo.value = task_data.memo;
+            }
+        }).send(formdata);
+    },
 }
