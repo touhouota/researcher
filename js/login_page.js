52450d26e8e3dafe0e9721330d897be3a08cdd82
touhouota <touhouota.register@gmail.com>
2018-01-19 16:30:01 +0900

    ページを開いたとき、user_idを持っているかに応じてページ遷移をするかどうかを判定してみた。

--- js/login_page.js
+++ js/login_page.js
@@ -1,4 +1,8 @@
 window.onload = function() {
 	let button = document.getElementById("login_button");
 	button.addEventListener("click", Login.check_account);
+
+	if (Base.get_cookie("user_id")) {
+		window.location = "/b1013179/researcher/main.html";
+	}
 }
--- js/main_page.js
+++ js/main_page.js
@@ -1,44 +1,43 @@
 window.onload = function() {
-	// user_idがあるかを確認
-	if (!Base.get_cookie("user_id")) {
-		// なければ、ログインページに飛ぶ
-		window.location = "/b1013179/researcher";
-	}
+    // user_idがあるかを確認
+    if (!Base.get_cookie("user_id")) {
+        // なければ、ログインページに飛ぶ
+        window.location = "/b1013179/researcher";
+    }
 
-	let _template_button = document.getElementById("task_button_template");
-	let template_button = document.importNode(_template_button.content, true);
+    let _template_button = document.getElementById("task_button_template");
+    let template_button = document.importNode(_template_button.content, true);
 
-	let my_areas = document.getElementsByClassName(Base.get_cookie("user_id"));
-	let i = 0,
-		length = my_areas.length;
-	let event_targets, j = 0,
-		target_size;
+    let my_areas = document.getElementsByClassName(Base.get_cookie("user_id"));
+    let i = 0,
+        length = my_areas.length;
+    let event_targets, j = 0,
+        target_size;
 
-	for (i = 1; i < length; i += 1) {
-		button = template_button.cloneNode(true).querySelector("button");
-		// console.log(button);
+    for (i = 1; i < length; i += 1) {
+        button = template_button.cloneNode(true).querySelector("button");
+        // console.log(button);
 
-		button.addEventListener("click", Events.create_form);
-		my_areas[i].appendChild(button);
+        button.addEventListener("click", Events.create_form);
+        my_areas[i].appendChild(button);
 
-		// 進捗更新ボタンのイベントを定義
-		my_areas[i].querySelector(".plus").addEventListener("click", Events.progress_plus);
-		my_areas[i].querySelector(".minus").addEventListener("click", Events.progress_minus);
+        // 進捗更新ボタンのイベントを定義
+        my_areas[i].querySelector(".plus").addEventListener("click", Events.progress_plus);
+        my_areas[i].querySelector(".minus").addEventListener("click", Events.progress_minus);
 
-		event_targets = my_areas[i].querySelectorAll("input[type=number], textarea");
-		for (j = 0, target_size = event_targets.length; j < target_size; j += 1) {
-			console.log(event_targets[i]);
-			event_targets[j].addEventListener("change", Events.form_change);
-		}
-	}
+        event_targets = my_areas[i].querySelectorAll("input[type=number], textarea");
+        for (j = 0, target_size = event_targets.length; j < target_size; j += 1) {
+            event_targets[j].addEventListener("change", Events.form_change);
+        }
+    }
 
-	// タスクの情報を取得する
-	let query = "?cmd=get_task";
-	Base.create_request("GET", Base.request_path + query, function() {
-		let response = JSON.parse(this.responseText);
-		console.table(response.data);
-		if (response.ok) {
-			TaskForm.insert_info(response.data);
-		}
-	}).send(null);
+    // タスクの情報を取得する
+    let query = "?cmd=get_task";
+    Base.create_request("GET", Base.request_path + query, function() {
+        let response = JSON.parse(this.responseText);
+        console.table(response.data);
+        if (response.ok) {
+            TaskForm.insert_info(response.data);
+        }
+    }).send(null);
 }
